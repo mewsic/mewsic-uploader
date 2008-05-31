b43e00372a0cf081f49aad8105801541b75dafdd
Marcello Barnaba <vjt@openssl.it>
2008-05-31 00:32:07 +0000

    multitrack: removed debug statements
    git-svn-id: file:///srv/svn/myousica/myousica/multitrack_server@631 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- app/controllers/sox_controller.rb
+++ app/controllers/sox_controller.rb
@@ -6,28 +6,22 @@ class SoxController < ApplicationController
 
   
   def export_song
-    doc_line = "<song><track><filename>uno.mp3</filename><volume>0.5</volume><loop>0</loop><balance>-0.2</balance></track><track><filename>due.mp3</filename><volume>0.1</volume><loop>0</loop><balance>0</balance><time_shift>0</time_shift></track></song>"
-        
     user_id = params[:user]
     output_name = "#{params[:user]}_#{Time.now.to_i.to_s}"
-    #tracklist = Tracklist.new(params[:song], output_name)
-    tracklist = Tracklist.new(doc_line, output_name)
+    tracklist = Tracklist.new(params[:song], output_name)
     
     # aspetto che la tracklist finisca...
     while tracklist.running?
-      sleep 2
+      sleep 1
     end
-    
     
     # ho i comandi, faccio partire il worker
     run_sox_worker(tracklist.commands, user_id)        
   end
   
   def status(worker = nil)
-    
     worker_key = params[:worker] || worker
     worker_info = MiddleMan.worker(:sox_worker, worker_key ).ask_status
-    
     if worker_info[:status] == 'finished'
       MiddleMan.delete_worker(:worker => :sox_worker, :job_key => worker_key)
     end 
@@ -39,25 +33,17 @@ class SoxController < ApplicationController
   
 protected
   def run_sox_worker(commands, user_id)        
-    
     worker_key = "#{user_id.to_s}_#{Time.now.to_i.to_s}"  
-
     worker_args = {:worker_key => worker_key, :output_name => @output_name}
-    
     MiddleMan.new_worker(:worker => :sox_worker, :job_key => worker_key, :data => worker_args) 
     
-    
-    
     MiddleMan.worker(:sox_worker, worker_key).start(commands)
-    
     render :xml => worker_status(worker_key, "idle")
-    
   end
   
   def worker_status(worker_key, w_status= nil)
     status_worker = w_status || status(worker_key) 
     return "<response><worker>#{worker_key}</worker><worker_status>#{status_worker}</worker_status></response>"
   end
-
 
 end
