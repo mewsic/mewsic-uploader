cd84cb45576aebba92cbcbcad038491a7ed752a6
Marcello Barnaba <vjt@openssl.it>
2008-06-30 21:30:55 +0000

    multitrack_server: rewrote XML parsing routines for track requests
    git-svn-id: file:///srv/svn/myousica/myousica/multitrack_server@766 1758279c-6b0d-4dd2-8212-40bfc25400ca

---
+++ lib/tracklist.rb
@@ -0,0 +1,53 @@
+require 'rexml/document'
+
+class TracklistError < StandardError
+end
+
+class Tracklist < Array
+  # <song>
+  #   <track id="2" volume="0.5" balance="0" filename="7f095f59b1a167a01822e098336af509.mp3"/>
+  #   <track id="15" volume="0.5"  balance="0" filename="05dacfb908ee316529a363dec51afa1e.mp3"/>
+  # </song>
+  
+  def initialize(xml)
+    doc = REXML::Document.new(xml)
+    
+    raise TracklistError, 'invalid XML' if doc.root.nil? || doc.root.name != 'song'
+
+    doc.root.elements.each('track') do |track|
+      self.push Track.new(track.attributes.symbolize_keys)
+    end
+  end
+end
+
+class Track
+  Attributes = [:id, :filename, :volume, :balance]
+
+  def initialize(attributes)
+    @attributes = attributes
+    @attributes.assert_valid_keys *Attributes
+
+    unless Attributes.all? { |attr| @attributes.has_key? attr }
+      raise TracklistError, 'incomplete track'
+    end
+
+    @attributes[:filename] = File.join(MP3_OUTPUT_DIR, @attributes[:filename])
+    unless File.exists? @attributes[:filename]
+      raise TracklistError, 'file not found'
+    end
+
+    @attributes[:id] = @attributes[:id].to_i
+    @attributes[:volume] = @attributes[:volume].to_f
+    @attributes[:balance] = @attributes[:balance].to_f
+  end
+
+  private
+    def method_missing(meth, *args, &block)
+      if @attributes.has_key? meth
+        @attributes[meth]
+      else
+        super(meth, *args, &block)
+      end
+    end
+end
+
