4003f2d38b5483d2130a44b97575b51239aa0fb4
Marcello Barnaba <vjt@openssl.it>
2008-06-30 21:29:12 +0000

    multitrack_server: created the Executable class, inherited by ffmpeg command class
    git-svn-id: file:///srv/svn/myousica/myousica/multitrack_server@764 1758279c-6b0d-4dd2-8212-40bfc25400ca

---
+++ lib/executable.rb
@@ -0,0 +1,27 @@
+class Executable
+  attr_reader :status
+
+  def run
+    return unless @status == :idle
+
+    @status = :running
+    puts "Executing #{self.to_cmd}"
+    @pid = fork { exec(self.to_cmd) }
+
+    return self
+  end
+
+  def running?
+    if @pid && Process.wait(@pid, Process::WNOHANG)
+      @pid = nil
+      @status = File.exist?(@output) ? :finished : :error
+    end
+
+    @status == :running
+  end
+
+  def success?
+    @status == :finished
+  end
+end
+
--- lib/ffmpeg.rb
+++ lib/ffmpeg.rb
@@ -1,6 +1,6 @@
-class FFmpeg
-  attr_reader :status
+require 'executable'
 
+class FFmpeg < Executable
   def initialize(input, output)
     @input, @output = input, output
     @status = File.exists?(@input) ? :idle : :error
@@ -9,25 +9,4 @@ class FFmpeg
   def to_cmd
     "ffmpeg -i #@input -ar #{MP3_FREQ} -ab #{MP3_RATE} -ac #{MP3_CHANNELS} #{MP3_OVERWRITE ? '-y' : ''} #@output"
   end
-
-  def run
-    return unless @status == :idle
-
-    @status = :running
-    @pid = fork { exec(self.to_cmd) }
-  end
-
-  def alive?
-    if @pid && Process.wait(@pid, Process::WNOHANG)
-      @pid = nil
-      @status = File.exist?(@output) ? :finished : :error
-    end
-
-    @status == :running
-  end
-
-  def success?
-    @status == :finished
-  end
-
 end
