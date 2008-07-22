eb40eb3195bce72522eb8122956bcfa7ee16ca80
Marcello Barnaba <vjt@openssl.it>
2008-07-22 13:35:30 +0000

    multitrack server: check for output file size in the Executable class
    git-svn-id: file:///srv/svn/myousica/myousica/multitrack_server@1068 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- lib/executable.rb
+++ lib/executable.rb
@@ -14,7 +14,7 @@ class Executable
   def running?
     if @pid && Process.wait(@pid, Process::WNOHANG)
       @pid = nil
-      @status = File.exists?(@output) ? :finished : :error
+      @status = (File.exists?(@output) && File.size(@output) > 0) ? :finished : :error
     end
 
     @status == :running
