03e038bc9eb0e55e0649de3dce7b1678ba52fe38
Marcello Barnaba <vjt@openssl.it>
2008-07-28 14:27:03 +0000

    multitrack server: ffmpeg refactoring
    git-svn-id: file:///srv/svn/myousica/myousica/multitrack_server@1188 1758279c-6b0d-4dd2-8212-40bfc25400ca

--- lib/ffmpeg.rb
+++ lib/ffmpeg.rb
@@ -3,25 +3,23 @@ require 'executable'
 class FFmpeg < Executable
   def initialize(input, output)
     @input, @output = input, output
+    @format = 'mp3'
+    @quality = "-ab #{MP3_RATE * 1024}"
+    @quality << " -aq #{MP3_QUALITY}" if MP3_VBR
+    @overwrite = MP3_OVERWRITE ? '-y' : ''
+
     error unless File.exists?(@input)
   end
 
   def to_cmd
-    quality = "-ab #{MP3_RATE * 1024}"
-    quality << " -aq #{MP3_QUALITY}" if MP3_VBR
-    overwrite = MP3_OVERWRITE ? '-y' : ''
-
-    "ffmpeg -i #@input -ar #{MP3_FREQ} -ac #{MP3_CHANNELS} #{quality} #{overwrite} #@output"
+    "ffmpeg -i #@input -ar #{MP3_FREQ} -ac #{MP3_CHANNELS} #@quality #@overwrite -f #@format #@output"
   end
 end
 
-class Wavepass < Executable
+class Wavepass < FFmpeg
   def initialize(input, output)
-    @input, @output = input, output
-    error unless File.exists?(@input)
-  end
-
-  def to_cmd
-    "ffmpeg -i #@input -f wav -y #@output"
+    super(input, output)
+    @format = 'wav'
+    @overwrite = '-y'
   end
 end
